a493f057e09350b5680eebe380c970e7a93de85f
SweSweNyein <sweswe4720@gmail.com>
2023-03-03 15:45:04 +0630

    admin login register design

--- resources/views/auth/admin/adminreset.blade.php
+++ resources/views/auth/admin/adminreset.blade.php
@@ -1,63 +1,217 @@
 @extends('layouts.app')
+@section('title','Kathit | Password Reset')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Password Reset') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ url('/password/adminreset')}}">
-                        @csrf
-                        <input type="hidden" name="token" value="{{ $token }}">
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="name" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                          <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                          <div class="col-md-6">
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                            @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                          </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<div class="me-0 loginregistercontainer p-0">
+  <div class="d-none d-md-block">
+    <div class="container-fluid row gx-0 gx-md-3">
+      <div class="col-12 col-md-6 admin-login-bg">
+        <div class="admin-login-logo">
+          <img src="{{ url('/images/logos/adminlogin.png') }}" alt="Kathit Login" class="login-photo">
+          <h4>WELCOME FROM KATHIT</h4>
         </div>
+      </div>
+      <div class="col-12 col-md-6 px-4 px-md-5 position-relative">
+        <div class="admin-login-form">
+          <h3 class="mb-4 py-2 lr-head">Admin <span class="highlight">Password Reset</span></h3>
+          <form method="POST" action="{{ url('/password/adminreset')}}">
+              @csrf
+              <input type="hidden" name="token" value="{{ $token }}">
+              <div class="mb-3">
+                <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="name" autofocus placeholder="Email Address">
+
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+              </div>
+
+              <div class="mb-3">
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="New Password">
+
+                @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+              </div>
+
+              <div class="mb-3">
+                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Confirm Password">
+              </div>
+              <div class="mb-0">
+                <button type="submit" class="lr-submit">
+                    {{ __('Reset Password') }}
+                </button>
+              </div>
+          </form>
+        </div>
+      </div>
     </div>
+  </div>
+  <div class="d-block d-md-none">
+    <div class="container-fluid row gx-0 gx-md-3 adminlogin-mb">
+      <img src="{{ url('/images/logos/logo.png') }}" alt="Kathit Login" class="col-12 login-photo-mb">
+      <div class="col-12 col-md-6 px-4 px-md-5">
+        <h3 class="mb-4 py-2 lr-head">Admin <span class="highlight">Password Reset</span></h3>
+        <form method="POST" action="{{ url('/password/adminreset')}}">
+            @csrf
+            <input type="hidden" name="token" value="{{ $token }}">
+            <div class="mb-3">
+              <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="name" autofocus placeholder="Email Address">
+
+              @error('email')
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                  </span>
+              @enderror
+            </div>
+
+            <div class="mb-3">
+              <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="New Password">
+
+              @error('password')
+              <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+              </span>
+              @enderror
+            </div>
+
+            <div class="mb-3">
+              <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Confirm Password">
+            </div>
+            <div class="mb-0">
+              <button type="submit" class="lr-submit">
+                  {{ __('Reset Password') }}
+              </button>
+            </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
 </div>
+
 @endsection
+@push('styles')
+  <style>
+    .highlight {
+      color: #d32f2f;
+    }
+    .login-photo {
+      width: 180px;
+    }
+    .login-photo-mb {
+      width: 200px;
+      margin: 30px auto 30px auto;
+      display: block;
+    }
+    .admin-login-bg {
+      background: #d32f2f;
+      height: 100vh;
+      position: relative;
+    }
+    .admin-login-bg .admin-login-logo {
+      position: absolute;
+      top: 45%;
+      left: 50%;
+      -ms-transform: translate(-50%, -50%);
+      transform: translate(-50%, -50%);
+    }
+    .admin-login-logo img {
+      margin: 0 auto;
+      display: block;
+      margin-bottom: 20px;
+    }
+    .admin-login-logo h4 {
+      text-align: center;
+      color: #fff;
+      font-weight: 900;
+    }
+    .admin-login-form {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      -ms-transform: translate(-50%, -50%);
+      transform: translate(-50%, -50%);
+      width: 62% !important;
+    }
+    .adminlogin-mb {
+      position: absolute;
+      top: 45%;
+      left: 50%;
+      -ms-transform: translate(-50%, -50%);
+      transform: translate(-50%, -50%);
+    }
+    .loginregistercontainer {
+      /* width: 100%; */
+      /* padding: 60px 80px;
+      border: 1px solid #3330;
+      box-shadow: 0px 0px 7px 1px #d32f2f26;
+      border-radius: 5px;
+      margin: 0 auto; */
+    }
+    .loginregistercontainer>div{
+      /* width: 100%;
+      margin-right: 20%; */
+    }
+    .lr-sub-head {
+      color: #3d0000;
+    }
+    .lr-head {
+      color: #3d0000;
+      font-weight: bold;
+    }
+    .lr-submit {
+      width: 100%;
+      background: #d32f2f;
+      border: 0px;
+      color: #fff;
+      padding: 10px;
+    }
+    .lr-submit:hover {
+      background: #3d0000;
+    }
+    .loginregistercontainer #email, .loginregistercontainer #password, 
+    .loginregistercontainer #name,
+    .loginregistercontainer #password-confirm  {
+      background: #f3f3f3;
+      border: 1px solid #f3f3f3;
+      padding: 10px;
+      border-radius: 0;
+    }
+    .forget-pass {
+      color: #d32f2f;
+    }
+    .forget-pass:hover {
+      color: #3d0000;
+    }
+    .register-link {
+      color: #3d0000;
+      font-weight: bold;
+    }
+
+    @media screen and (max-width: 768px) {
+      .loginregistercontainer {
+        width: 100%;
+        padding: 20px 0;
+        border: 0;
+        box-shadow: none;
+      }
+      .loginregistercontainer>div{
+        width: 100%;
+        margin-right: 0px;
+      }
+      .login-photo {
+        width: 140px;
+        margin-bottom: 40px;
+      }
+    }
+    @media screen and (min-width: 768px) and (max-width: 1053px){
+      .loginregistercontainer {
+        width: 100%;
+      }
+    }
+  </style>
+@endpush
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,73 +1,41 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="icon" type="image/x-icon" href="{{ url('/images/logos/favicon.png') }}">
 
-    <title>Kathit Fashion</title>
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/app.css') }}"/>
+    <link rel="stylesheet" href="{{url('test/css/fancybox.css')}}"/>
+    <link rel="stylesheet" href="{{url('test/css/swiper-bundle.min.css')}}"/>
+    <link rel="stylesheet" href="{{url('backend/plugins/fontawesome-free/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{url('backend/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+     
+    </style>
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm d-none">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<body style="background: #fff;">
+<div id="app">
+    {{-- @include('layouts.frontend.menu') --}}
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+    @yield('content')
 
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+    {{-- @include('layouts.frontend.footer') --}}
+</div>
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{url('test/js/fancybox.js')}}"></script>
+<script src="{{url('test/js/swiper-bundle.min.js')}}"></script>
+<script src="{{url('backend/plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{url('backend/plugins/sweetalert2/sweetalert2.all.js')}}"></script>
+@stack('styles')
+@stack('scripts')
 </body>
 </html>
+
+
+
+
